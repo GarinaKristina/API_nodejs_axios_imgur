3372f1fd6e3ba12ef0a50adffcbe9d84fcfa2a9e
Kristina <tinka_e@mail.ru>
2023-03-27 19:19:58 +0300

    add config factory

--- config/Configs.js
+++ config/Configs.js
@@ -1,3 +1,12 @@
-import GetConfig from './GetConfig';
-import PostConfig from './PostConfig';
-import DeleteConfig from './DeleteConfig';
+import GetConfig from './GetConfig.js';
+import PostConfig from './PostConfig.js';
+import DeleteConfig from './DeleteConfig.js';
+
+class Configs {
+  constructor() {
+    this.GetConfig = GetConfig;
+    this.PostConfig = PostConfig;
+    this.DeleteConfig = DeleteConfig;
+  }
+}
+export default new Configs();
--- test/app.js
+++ test/app.js
@@ -1,19 +1,17 @@
 import axios from 'axios';
-import PostConfig from '../config/PostConfig.js';
-import GetConfig from '../config/GetConfig.js';
-import DeleteConfig from '../config/DeleteConfig.js';
+import Configs from '../config/Configs.js';
 import { expect } from 'chai';
 
 describe('API testing', async () => {
   it('In GET request, status code is equal 200', async () => {
-    await axios(GetConfig.getConfig).then((response) => {
+    await axios(Configs.GetConfig.getConfig).then((response) => {
       let statusResult = response.data.status;
       expect(statusResult).to.equal(200);
     });
   });
 }),
-  it('In POST request, status code is equal 200 and type of image includes "gif"', async () => {
-    await axios(PostConfig.postConfig).then((response) => {
+  it('In POST request, status code is equal "200" and type of image includes "gif"', async () => {
+    await axios(Configs.PostConfig.postConfig).then((response) => {
       let statusResult = response.data.status;
       let imageType = response.data.data.type;
       expect(statusResult).to.equal(200);
@@ -21,7 +19,7 @@ describe('API testing', async () => {
     });
   }),
   it('In DELETE request, status code is equal "true""', async () => {
-    await axios(DeleteConfig.deleteConfig).then((response) => {
+    await axios(Configs.PostConfig.postConfig).then((response) => {
       let deletedStatus = response.data.success;
       expect(deletedStatus).to.equal(true);
     });
