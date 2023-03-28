2eb559aae15c69337e4090c7c6d6ff4d70d9ded4
Kristina <tinka_e@mail.ru>
2023-03-28 15:31:30 +0300

    update

--- test/app.js
+++ test/app.js
@@ -4,29 +4,25 @@ import { expect } from 'chai';
 
 describe('API testing', async () => {
   it('In GET request, status code is equal 200', async () => {
-    await axios(ConfigsFactory.Get.config).then((response) => {
-      let statusResult = response.data.status;
-      expect(statusResult).to.equal(200);
-    });
-  });
-}),
-  it('In POST request, status code is equal "200" and type of image includes "gif"', async () => {
-    await axios(ConfigsFactory.Post.config).then((response) => {
-      let statusResult = response.data.status;
-      let imageType = response.data.data.type;
-      expect(statusResult).to.equal(200);
-      expect(imageType).to.include('gif');
-    });
-  }),
-  it('In DELETE request, status code is equal "true""', async () => {
-    await axios(ConfigsFactory.Delete.config).then((response) => {
-      let deletedStatus = response.data.success;
-      expect(deletedStatus).to.equal(true);
-    });
-  });
-it('In PUT request, status code is equal "200"', async () => {
-  await axios(ConfigsFactory.Put.config).then((response) => {
+    const response = await axios(ConfigsFactory.Get.config);
     let statusResult = response.data.status;
     expect(statusResult).to.equal(200);
   });
-});
+}),
+  it('In POST request, status code is equal "200" and type of image includes "gif"', async () => {
+    const response = await axios(ConfigsFactory.Post.config);
+    let statusResult = response.data.status;
+    let imageType = response.data.data.type;
+    expect(statusResult).to.equal(200);
+    expect(imageType).to.include('gif');
+  }),
+  it('In DELETE request, status code is equal "true""', async () => {
+    const response = await axios(ConfigsFactory.Delete.config);
+    let deletedStatus = response.data.success;
+    expect(deletedStatus).to.equal(true);
+  }),
+  it('In PUT request, status code is equal "200"', async () => {
+    const response = await axios(ConfigsFactory.Put.config);
+    let statusResult = response.data.status;
+    expect(statusResult).to.equal(200);
+  });
